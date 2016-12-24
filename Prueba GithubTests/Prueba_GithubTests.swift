c174cbc74c0823031231b07fe342139c1a21638f
Luis Venegas <luisvenegas@luiss-mac.local>
2016-12-24 15:05:20 -0300

    Initial Commit

---
+++ Prueba GithubTests/Prueba_GithubTests.swift
@@ -0,0 +1,36 @@
+//
+//  Prueba_GithubTests.swift
+//  Prueba GithubTests
+//
+//  Created by Luis Venegas on 12/24/16.
+//  Copyright © 2016 Luis Venegas. All rights reserved.
+//
+
+import XCTest
+@testable import Prueba_Github
+
+class Prueba_GithubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
